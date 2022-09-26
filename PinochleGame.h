80c0686f9a2c9dfdf3ea6cda707d21ce52cd5d27
tessjenkins <jenkinstess@wustl.edu>
2022-09-25 23:14:41 -0500

    halfway in 12

---
+++ PinochleGame.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "PinochleDeck.h"
+#include "CardSet_T.h"
+
+class PinochleGame : public Game{
+    public:
+        PinochleGame(int argc, const char *argv[]);
+        virtual int play();
+
+    protected:
+        PinochleDeck _pDeck; 
+        std::vector< CardSet_T<PinochleRank, Suits> > _curHand;
+        virtual void _deal();
+
+    private:
+}
