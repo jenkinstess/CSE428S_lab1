0910977d480810301bc16c671dc2886fb08fe866
tessjenkins <jenkinstess@wustl.edu>
2022-09-30 12:56:53 -0500

    main

--- lab1.cpp
+++ lab1.cpp
@@ -10,8 +10,32 @@
 #include <iostream>
 #define SUCCESS 0
 
-int main(int argc, const char* argv[]){
+#define PINOCHLEPLAYERS 6 
+#define MINHOLDEMPLAYERS 4
+#define MAXHOLDEMPLAYERS 11
+#define INCORRECTINPUT 1
+#define NULLGAME 2
+
+int main(int argc, const char* argv[]) {
     
+    if ((argv[1].equals("Pinochle") && argc == PINOCHLEPLAYERS)
+        || (argv[1].equals("HoldEm") && (argc >= MINHOLDEMPLAYERS && argc <= MAXHOLDEMPLAYERS))) {
+            
+            std::shared_ptr<Game> gamePtr = create(argc, argv);
+
+            if (gamePtr) {
+                return gamePtr->play();
+            }
+            else {
+                std::cout << "No game created" << std::endl;
+                return NULLGAME;
+            }
+    }
+    else{
+        std::cout << "usage: filename gamename playername1 playername2 ..." << std::endl;
+        return INCORRECTINPUT;
+    }
+
     //HoldEmDeck h;
     PinochleDeck p;
 
@@ -26,4 +50,18 @@ int main(int argc, const char* argv[]){
     g.play();
 
     return SUCCESS; 
+}
+
+std::shared_ptr<Game> create(int argc, const char* argv[]) {
+    
+    std::shared_ptr<Game> game1;
+
+    if (argv[1].equals("Pinochle")) {
+        game1 = std::make_shared<PinochleGame>(argc, argv);
+    }
+    else (argv[1].equals("HoldEm")) {
+        game1 = std::make_shared<HoldEmGame>(argc, argv);
+    }
+
+    return game1;
 }
