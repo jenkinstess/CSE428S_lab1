0ba2325bec3a162cab1639fab54bf338c523ca5d
Sam Saxon <s.saxon@wustl.edu>
2022-09-30 13:15:56 -0400

    13 progress

--- HoldEmGame.cpp
+++ HoldEmGame.cpp
@@ -1,1 +1,34 @@
 #include "HoldEmGame.h"
+
+HoldEmGame::HoldEmGame(int argc, const char* argv[]) : Game(argc, argv) {
+    for(int i = 0; i < argc-2; ++i){
+        CardSet<HoldEmRank, Suits> cs;
+        hands.push_back(cs);
+    }
+
+    state = HoldEmState::preflop;
+}
+
+
+void HoldEmGame::deal() {
+
+
+}
+
+
+void HoldEmGame::play() {
+
+
+}
+
+
+void HoldEmGame::print_players() {
+
+
+}
+
+
+void HoldEmGame::collect_all() {
+
+    
+}
--- HoldEmGame.h
+++ HoldEmGame.h
@@ -8,7 +8,14 @@ enum class HoldEmState{
 class HoldEmGame : public Game {
 
 public:
-
+    
+    virtual int play();
 protected:
+    HoldEmDeck deck; 
+    HoldEmState state;
+    virtual void deal();
+    std::vector< CardSet<HoldEmRank, Suits> > hands;
+    void print_players();
+    void collect_all();
 
 };
